86be2bf5c917beb57dd92f9bec5934d18ab5450e
Alex <choijeaho86@gmail.com>
2024-01-12 15:28:11 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Document를 설정
+  const options = new DocumentBuilder()
+    .setTitle('typeorm relation excercise')
+    .build();
+
+  // Swagger Document의 문서를 api(/api-docs)로 설정할수 있게 셋팅
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
